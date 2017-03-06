e907194e21158c5de1390d88216a2daecb889d64
Carlos Farinha <09-10270@usb.ve>
2017-03-06 15:27:02 -0400

    aun falta comprimir imagenes

--- k-means.py
+++ k-means.py
@@ -1,34 +1,64 @@
 import numpy as np
+import random
+import matplotlib.pyplot as plt
  
-def cluster_points(X, mu):
+def puntos_del_cluster(casos, media):
     clusters  = {}
-    for x in X:
-        bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
-                    for i in enumerate(mu)], key=lambda t:t[1])[0]
+    for x in casos:
+        bestmukey = min([(i[0], np.linalg.norm(x-media[i[0]])) \
+                    for i in enumerate(media)], key=lambda t:t[1])[0]
         try:
             clusters[bestmukey].append(x)
         except KeyError:
             clusters[bestmukey] = [x]
     return clusters
  
-def reevaluate_centers(mu, clusters):
-    newmu = []
+def iteracion(media, clusters):
+    nuevo_m = []
     keys = sorted(clusters.keys())
     for k in keys:
-        newmu.append(np.mean(clusters[k], axis = 0))
-    return newmu
- 
-def has_converged(mu, oldmu):
-    return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
- 
-def find_centers(X, K):
-    # Initialize to K random centers
-    oldmu = random.sample(X, K)
-    mu = random.sample(X, K)
-    while not has_converged(mu, oldmu):
-        oldmu = mu
-        # Assign all points in X to clusters
-        clusters = cluster_points(X, mu)
-        # Reevaluate centers
-        mu = reevaluate_centers(oldmu, clusters)
-    return(mu, clusters)
+        nuevo_m.append(np.mean(clusters[k], axis = 0))
+    return nuevo_m
+  
+def centrar(casos, K):
+    viejo_m = random.sample(casos, K)
+    media = random.sample(casos, K)
+    while not (set([tuple(a) for a in media]) == set([tuple(a) for a in viejo_m])):
+        viejo_m = media
+        clusters = puntos_del_cluster(casos, media)
+        media = iteracion(viejo_m, clusters)
+    return(media, clusters)
+
+
+def leer_archivo(N, k):
+
+    n = float(N)/k
+    casos = []
+    for i in range(k):
+        c = (random.uniform(-1, 1), random.uniform(-1, 1))
+        s = random.uniform(0.05,0.5)
+        x = []
+        while len(x) < n:
+            a, b = np.array([np.random.normal(c[0], s), np.random.normal(c[1], s)])
+            if abs(a) < 1 and abs(b) < 1:
+                x.append([a,b])
+        casos.extend(x)
+    casos = np.array(casos)[:N]
+    return casos
+
+k = 6
+lectura = np.loadtxt('bezdekIris.data',dtype=str,delimiter=",")
+casos = lectura[:,0:4]
+resultados = lectura[:,4]
+
+
+c = (centrar(casos,k))[1]
+p = promedio(c,k)
+
+label_color = {0 : '#FF0000',1 : '#FFFF00',2 : '#0033CC',3 : '#00CC00',4 : '#660099',5 : '#FF9900',6 : '#000000',}
+
+
+for i in range(k):
+    plt.scatter(np.array(c[i])[:,0],np.array(c[i])[:,1],s=50, alpha = 0.5 ,c=label_color[i])
+    plt.scatter(np.array(p[i])[0],np.array(p[i])[1],s=200, alpha = 1 ,c=label_color[i], marker="*")
+plt.show()
