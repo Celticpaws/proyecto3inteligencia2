fee13d692bbecd80501f7b6454c649016e3bbaa5
Carlos Farinha <09-10270@usb.ve>
2017-03-06 18:26:54 -0400

    Seleccione k

--- k-means.py
+++ k-means.py
@@ -45,18 +45,17 @@ def to_ternary(row):
         return np.append(row[:-1],[0,0,1],axis=0)
 v_to_ternary = np.vectorize(to_ternary)
 
-k = 3
+try:
+    k = int(raw_input("Introduzca un k: "))
+except Exception as e:
+    k = 3
+print(k)
+
 lectura = np.loadtxt('bezdekIris.data',dtype=str,delimiter=",")
 bin_dat = np.matrix(map(to_binary,lectura),dtype=np.float)
-
-print "Casos:"
-print bin_dat
-
 c = (centrar(np.array(bin_dat),k))[1]
-
+print c
 label_color = {0 : '#FF0000',1 : '#FFFF00',2 : '#0033CC',3 : '#00CC00',4 : '#660099',5 : '#FF9900',6 : '#000000',}
-
-
 for i in range(k):
     plt.scatter(np.array(c[i])[:,0],np.array(c[i])[:,1],s=50, alpha = 0.5 ,c=label_color[i])
 plt.show()
